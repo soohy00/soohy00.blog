b9119009db3feb485d38eed37a34fe3ed4f1d085
soohy00 <lixx7273@gmail.com>
2025-01-14 00:13:07 +0900

    Update blog configuration and add profile image; modify Notion sync script

--- blog-config.js
+++ blog-config.js
@@ -2,10 +2,15 @@ module.exports = {
   title: "Soohy00",
   description: "안녕하세요. 제 블로그 글을 통해 많은 정보 얻어가셨으면 좋겠습니다.",
   author: "Soohy00",
-  siteUrl: "https://soohy00.github.io/gatsby-starter-hoodie/",
+  siteUrl: "https://soohy00.github.io",
   links: {
     github: "https://github.com/soohy00",
   },
+  utterances: {
+    repo: "soohy00/soohy00.github.io",
+    type: "pathname",
+  },
+  profileImageFileName: "profile.png",
   useAbout: true,
   // See https://giscus.app/
   giscus: {
